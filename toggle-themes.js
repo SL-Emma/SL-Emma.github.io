7bb39655d2015a2dddf99f9a201da877f78a0620
emma <erichte3@hs-mittweida.de>
2025-08-24 08:10:36 +0200

    added research section and added functionality for theme change

--- toggle-themes.js
+++ toggle-themes.js
@@ -1,25 +1,48 @@
+document.querySelectorAll("#toggle-theme, #art-theme, #code-theme, #martial-theme").forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
 
-document.getElementById("toggle-theme").addEventListener("click", (e) => {
-    e.preventDefault();
+        const root = document.documentElement;
+        const themeImage = document.getElementById("theme-image");
+        const theme = e.target.getAttribute("href").substring(1); 
 
-    // check current background color and toggle
-    const root = document.documentElement;
-    const isMatcha = root.style.getPropertyValue("--bg-color") === "#010116";
+        if (theme === "matcha") {
+            root.style.setProperty("--bg-color", "#040b00ff");
+            root.style.setProperty("--bg-highlight-color", "#486213ff");
+            root.style.setProperty("--highlight-color", "#D4D0B9");
+            root.style.setProperty("--primary-font-color", "white");
+            root.style.setProperty("--box-color", "#192f0eff");   
+            if (themeImage) {
+                themeImage.src = "./images/me3.jpg"; 
+            }
+        } else if (theme === "art") {
+            root.style.setProperty("--bg-color", "#030022ff");
+            root.style.setProperty("--bg-highlight-color", "#160967ff");
+            root.style.setProperty("--highlight-color", "#3041d5ff");
+            root.style.setProperty("--primary-font-color", "#a99aedff");
+            root.style.setProperty("--box-color", "#140e3fff");   
+            if (themeImage) {
+                themeImage.src = "./images/art-theme.jpg"; 
+            }
 
-    if (isMatcha) {
-        // switch to greenish theme
-        root.style.setProperty("--bg-color", "#394931");
-        root.style.setProperty("--bg-highlight-color", "#90997F");
-        root.style.setProperty("--highlight-color", "#D4D0B9");
-        root.style.setProperty("--primary-font-color", "white");
-        root.style.setProperty("--box-color", "#394931");   
-    } else {
-        // switch back to original theme
-        root.style.setProperty("--bg-color", "#010116");
-        root.style.setProperty("--bg-highlight-color", "#1b033e");
-        root.style.setProperty("--highlight-color", "#a117ff");
-        root.style.setProperty("--primary-font-color", "rgb(164, 148, 190)");
-        root.style.setProperty("--box-color", "rgb(8, 0, 18)");
-    }
+        } else if (theme === "martial") {
+            root.style.setProperty("--bg-color", "#13000bff");
+            root.style.setProperty("--bg-highlight-color", "#50071dff");
+            root.style.setProperty("--highlight-color", "#d5303bff");
+            root.style.setProperty("--primary-font-color", "#f3c1d8ff");
+            root.style.setProperty("--box-color", "#27060dff");   
+            if (themeImage) {
+                themeImage.src = "./images/karate_me.jpg"; 
+            }
+        } else {
+            root.style.setProperty("--bg-color", "#010116");
+            root.style.setProperty("--bg-highlight-color", "#1b033e");
+            root.style.setProperty("--highlight-color", "#a117ff");
+            root.style.setProperty("--primary-font-color", "rgb(164, 148, 190)");
+            root.style.setProperty("--box-color", "rgb(8, 0, 18)");
+            if (themeImage) {
+                themeImage.src = "./images/me2_gre2.png"; 
+            }
+        }
+    });
 });
-
